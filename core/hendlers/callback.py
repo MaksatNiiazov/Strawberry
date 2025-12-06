265264f7f915c04794e47e20d7bbffe003cb83ea
Maksat Niiazov <103022478+MaksatNiiazov@users.noreply.github.com>
2025-12-07 01:55:16 +0600

    Add dependency requirements file

--- core/hendlers/callback.py
+++ core/hendlers/callback.py
@@ -1,29 +1,42 @@
 from telegram import Update
-from telegram.ext import CallbackContext, CallbackQueryHandler
+from telegram.ext import ContextTypes
 
-from core.texts.models.texts import MODEL_ORDER_WEBCAM, MODEL_ORDER_DEFAULT
+from core.texts.models.texts import MODEL_ORDER_DEFAULT, MODEL_ORDER_WEBCAM
 
-def check_model_experience(update: Update, context: CallbackContext):
+
+async def check_model_experience(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     call = update.callback_query
-    experience = call.data.split('_')[-1]
-    if experience == 'yes':
-        answer = 'sssssssssss'
-    elif experience == 'no':
-        answer = 'awwwedasd'
+    if not call:
+        return
+
+    experience = call.data.split("_")[-1]
+    if experience == "yes":
+        answer = "sssssssssss"
+    elif experience == "no":
+        answer = "awwwedasd"
     else:
-        answer = 'awwwedasd'
-    call.message.reply_text(answer)
-    call.answer()
+        answer = "awwwedasd"
 
-def model_order(update: Update, context: CallbackContext):
+    if call.message:
+        await call.message.reply_text(answer)
+
+    await call.answer()
+
+
+async def model_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     call = update.callback_query
-    experience = call.data.split('_')[-1]
-    if experience == 'webcam':
+    if not call:
+        return
+
+    experience = call.data.split("_")[-1]
+    if experience == "webcam":
         answer = MODEL_ORDER_WEBCAM
-    elif experience == 'default':
+    elif experience == "default":
         answer = MODEL_ORDER_DEFAULT
     else:
-        answer = 'awwwedasd'
+        answer = "awwwedasd"
 
-    call.message.reply_text(answer)
-    call.answer()
+    if call.message:
+        await call.message.reply_text(answer)
+
+    await call.answer()
