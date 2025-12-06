732bf3daad6eea7f6c6a5a92e1ca816ce0858563
Maksat Niiazov <103022478+MaksatNiiazov@users.noreply.github.com>
2025-12-07 05:54:27 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 TELEGRAM_API_KEY = os.environ.get("TELEGRAM_API_KEY")
 ADMIN_CHAT_ID = int(os.environ["ADMIN_CHAT_ID"]) if os.environ.get("ADMIN_CHAT_ID") else None
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH", "/telegram/webhook")
 HOST = os.environ.get("HOST", "0.0.0.0")
 PORT = int(os.environ.get("PORT", "8000"))
